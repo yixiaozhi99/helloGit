73aa5002da1fee574ae1075237262ec2d276d0cc
xiaoyu <1150296791@qq.com>
2020-09-01 20:41:34 +0800

    主干添加

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -10,6 +10,7 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("更新一");
         System.out.println("创建分支");
+        System.out.println("主干添加");
 
     }
 }
